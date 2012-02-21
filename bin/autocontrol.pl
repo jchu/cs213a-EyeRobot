1086dc6ccaeab94f233dbd48169c2717b60cd52c
Jonathan Chu <jrchu@cs.ucla.edu>
2012-02-21 00:13:58 -0800

    Move forward only

--- bin/autocontrol.pl
+++ bin/autocontrol.pl
@@ -5,14 +5,20 @@ use warnings;
 
 use Net::Telnet;
 use OEMModule3;
+use OEMModule3::Telnet;
+use IRobot;
+use IRobot::Telnet;
 
 use SDL;
 use SDL::Event;
 use SDLx::App;
 use SDLx::Controller;
 
+my $MAX_DISTANCE_SKEW = 1000;
+
 my $oem;
 my $robot;
+my $state;
 
 my $app = SDLx::Controller->new(
     dt      => 1, # movement step
@@ -21,20 +27,60 @@ my $app = SDLx::Controller->new(
 );
 
 $app->add_move_handler(\&check_status);
+$app->add_move_handler(\&move);
 
 init_components();
+center_robot();
+
 $app->run();
 
 $oem->off();
+$robot->off();
+
+sub move {
+    if( $state->{moveable} ) {
+        $robot->forward();
+    } else {
+        $robot->stop();
+    }
+}
+
+sub center_robot {
+
+    # simple. assume centered
+    my $dist = $oem->measure_distance();
+    
+    if( $dist ) {
+        $state->{distance_to_wall} = $dist;
+    } else {
+        warn "Sensors broken";
+        exit(1);
+    }
+
+    warn "Maintaining distance " . $state->{distance_to_wall} . " from wall\n";
+}
+
+sub recenter_robot {
+}
 
 
 sub check_status {
+
+    # Check sensors
     my $dist = $oem->measure_distance();
     if ($dist) {
-        warn $app->current_time . ": $dist\n";
+        print $app->current_time . ": $dist\n";
+
+        if( abs($state->{distance_to_wall} - $dist) > $MAX_DISTANCE_SKEW ) {
+            warn "Distance skew detected\n";
+            recenter_robot();
+        }
     } else {
         warn "error: " . $oem->error() . "\n";
     }
+
+    # Determine status
+
 }
 
 sub init_components {
@@ -59,4 +105,17 @@ sub init_components {
             comm => $oem_telnet
         });
     $oem->on();
+
+    my $robot_telnet = IRobot::Telnet->new(
+        server_name => $ENV{EYEROBOT_TELNET},
+        server_port => 2000,
+        telnet => $sock
+    );
+
+    $robot = IRobot->new({
+            comm_class => 'IRobot::Telnet',
+            server_name => $ENV{EYEROBOT_TELNET},
+            server_port => 2000,
+            comm => $robot_telnet
+        });
 }
